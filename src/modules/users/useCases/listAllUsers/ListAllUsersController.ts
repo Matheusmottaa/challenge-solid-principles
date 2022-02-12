2561b19c81730f071fcd322f6a66ecf3e8557a8a
Matheus Mota <matheusfelipe1918@gmail.com>
2022-02-12 19:20:08 -0300

    list all user controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      const id = user_id.toString();
+      const user = this.listAllUsersUseCase.execute({ user_id: id });
+      return response.status(200).json(user);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
